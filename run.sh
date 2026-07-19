cce0139cc292bbb93d5d29d4d138cdb3206452ba
Abhijit Karale <abhijitkarale8@gmail.com>
2026-07-19 20:46:55 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ vvp sim.out
 python3 vcd_plot.py waveform/uart_controller.vcd waveform/uart_controller_waveform.png \
     clk rst_n tx_start tx_data tx_busy tx_serial rx_data rx_valid --window 0 400
 echo "Done. Open waveform/uart_controller.vcd in GTKWave for full interactive waveform."
+    
