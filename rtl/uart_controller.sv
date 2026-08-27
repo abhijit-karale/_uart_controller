08447881147662fd67b2efd32d275c2820d96585
Abhijit Karale <abhijitkarale8@gmail.com>
2026-08-27 07:48:23 +0530

    Fix author comment formatting in uart_controller.sv

--- rtl/uart_controller.sv
+++ rtl/uart_controller.sv
@@ -3,7 +3,7 @@
  // Description : Simple UART controller (8 data bits, no parity, 1 stop bit).
  //               Contains independent transmitter (uart_tx) and receiver
  //               (uart_rx) sharing a common baud-rate generator.
-// Author      : Abhijit Karale
+ // Author      : Abhijit Karale
 // -----------------------------------------------------------------------------
 module uart_controller #(
     parameter CLK_FREQ  = 50_000_000,
